9dc711d237d517c36fe6cdf11ed5c3b0fa4dc11c
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-31 20:30:25 +0300

    add class LoginLocator.java

---
+++ src/test/java/by/onliner/LoginLocator.java
@@ -0,0 +1,9 @@
+package by.onliner;
+
+public class LoginLocator {
+    public static final String NICKNAME_OR_EMAIL_FIELD = "//div[@class='auth-form__line auth-form__line_condensed']//input[@type='text']";
+    public static final String PASSWORD_FIELD = "//div[@class='auth-form__line auth-form__line_condensed']//input[@type='password']";
+    public static final String SIGN_IN_BUTTON = "//button[@type='submit']";
+    public static final String EMPTY_NICKNAME_OR_EMAIL_ERROR_MESSAGE = "//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other']";
+    public static final String EMPTY_PASSWORD_ERROR_MESSAGE = "(//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other'])[last()]";
+}
